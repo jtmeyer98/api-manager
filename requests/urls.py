f6d0ddc048d7c16ee0778b5d1f3d27b019fd1696
jtmeyer98 <jtmeyer@uc.cl>
2024-05-03 12:09:58 -0400

    feat: agregar request line a la api

--- requests/urls.py
+++ requests/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PurchaseRequestViewSet
+from .views import PurchaseRequestViewSet, RequestLineViewSet
 
 router = DefaultRouter()
 router.register(r'purchase_requests', PurchaseRequestViewSet)
+router.register(r'request_lines', RequestLineViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- requests/views.py
+++ requests/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import PurchaseRequest
-from .serializers import PurchaseRequestSerializer
+from .models import PurchaseRequest, RequestLine
+from .serializers import PurchaseRequestSerializer, RequestLineSerializer
 
 class PurchaseRequestViewSet(viewsets.ModelViewSet):
     queryset = PurchaseRequest.objects.all()
     serializer_class = PurchaseRequestSerializer
+
+class RequestLineViewSet(viewsets.ModelViewSet):
+    queryset = RequestLine.objects.all()
+    serializer_class = RequestLineSerializer
