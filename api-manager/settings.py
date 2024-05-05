71d5ab4f115ac19df91cf9fd9b28718a6c1e386d
jtmeyer98 <jtmeyer@uc.cl>
2024-05-05 16:51:18 -0400

    feat: mejorar la vista de la api

--- api-manager/settings.py
+++ api-manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') if os.environ.get('SECRET_KEY') else '
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true' if os.environ.get('DEBUG') else True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ') if os.environ.get('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ') if os.environ.get('ALLOWED_HOSTS') else ['*']
 
 # Application definition
 
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'api-manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'api-manager'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
