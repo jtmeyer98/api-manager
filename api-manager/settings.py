bc794abaa3fe578aeaf2d184c763f9bb7e1bb415
jtmeyer98 <jtmeyer@uc.cl>
2024-05-06 16:55:52 -0400

    agregar db.sqlite3 al repo y render

--- api-manager/settings.py
+++ api-manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') if os.environ.get('SECRET_KEY') else '
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true' if os.environ.get('DEBUG') else True
 
-ALLOWED_HOSTS = ['api-manager-x6lx.onrender.com','https://dashboard-kxki.onrender.com/']
+ALLOWED_HOSTS = ['api-manager-x6lx.onrender.com','https://dashboard-kxki.onrender.com/','127.0.0.1']
 
 # Application definition
 
