c4e18421d016314bb48ef96f37bd19996fde8184
jtmeyer98 <jtmeyer@uc.cl>
2024-05-06 23:43:15 -0400

    fix: operaciones CRUD en admin

--- requests/serializers.py
+++ requests/serializers.py
@@ -4,7 +4,13 @@ from .models import PurchaseRequest, RequestLine
 class RequestLineSerializer(serializers.ModelSerializer):
     class Meta:
         model = RequestLine
-        fields = ['id', 'product', 'quantity', 'price', 'currency']
+        fields = ['product', 'quantity', 'price', 'currency', 'purchase_request']
+
+    def validate_purchase_request_id(self, value):
+        if not PurchaseRequest.objects.filter(id=value).exists():
+            raise serializers.ValidationError("PurchaseRequest con este ID no existe.")
+        return value
+
 
 class PurchaseRequestSerializer(serializers.ModelSerializer):
     request_lines = RequestLineSerializer(many=True, read_only=True)
