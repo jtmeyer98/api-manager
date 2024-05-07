d0386ce4a04853f39f09e8c8948850549008aae5
jtmeyer98 <jtmeyer@uc.cl>
2024-05-06 22:57:22 -0400

    feat: admin mejorado

--- requests/admin.py
+++ requests/admin.py
@@ -3,10 +3,12 @@ from .models import PurchaseRequest, RequestLine
 
 @admin.register(PurchaseRequest)
 class PurchaseRequestAdmin(admin.ModelAdmin):
-    list_display = ['requester', 'date', 'type', 'status'] 
+    list_display = ['requester', 'date', 'type', 'status']
     search_fields = ['requester', 'description']
+    list_filter = ['status', 'type']
 
 @admin.register(RequestLine)
 class RequestLineAdmin(admin.ModelAdmin):
     list_display = ['product', 'quantity', 'price', 'currency', 'purchase_request']
     search_fields = ['product']
+    list_filter = ['currency', 'purchase_request__status'] 
